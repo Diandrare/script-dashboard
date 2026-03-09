af1b027b7a42b555fc47de6191fd72cf82526482
Nevermind. <162158483+Diandrare@users.noreply.github.com>
2026-03-09 22:18:27 +0700

    -

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.use(express.static("public"))
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, ()=>{
+app.listen(PORT, "0.0.0.0", ()=>{
     console.log("Server running on port " + PORT)
 })
