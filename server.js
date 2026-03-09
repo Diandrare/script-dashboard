7c0b04811aa67521f54682befccc5be792e01e20
Nevermind. <162158483+Diandrare@users.noreply.github.com>
2026-03-09 22:14:02 +0700

    -

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+const express = require("express")
+const app = express()
 
+app.use(express.json())
+
+let users = []
+
+app.post("/log",(req,res)=>{
+    users.push(req.body)
+    console.log("User:", req.body)
+    res.send("ok")
+})
+
+app.get("/data",(req,res)=>{
+    res.json(users)
+})
+
+app.use(express.static("public"))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+    console.log("Server running on port " + PORT)
+})
