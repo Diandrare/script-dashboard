b61ba47303a60c16129173fed0ed0cb07da132d6
Nevermind. <162158483+Diandrare@users.noreply.github.com>
2026-03-10 01:23:40 +0700

    -

--- server.js
+++ server.js
@@ -5,33 +5,33 @@ app.use(express.json());
 
 let users = [];
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
   res.send("server running");
 });
 
-app.get("/log",(req,res)=>{
+app.get("/log", (req, res) => {
 
   const data = {
-    username:req.query.username,
-    userid:req.query.userid,
-    placeid:req.query.placeid,
-    time:req.query.time
+    username: req.query.username,
+    userid: req.query.userid,
+    placeid: req.query.placeid,
+    time: req.query.time
   };
 
   users.push(data);
 
-  console.log("User:",data);
+  console.log("User:", data);
 
   res.send("logged");
 
 });
 
-app.get("/data",(req,res)=>{
+app.get("/data", (req, res) => {
   res.json(users);
 });
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
 });
